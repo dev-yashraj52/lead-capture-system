ab91ebd59b9d4d98da86713c216dba7df8913177
Yash Raj <dev.yashraj52@gmail.com>
2026-06-05 21:01:20 +0530

    define logic to createNewLead in leadModels.js

--- backend/src/models/leadModels.js
+++ backend/src/models/leadModels.js
@@ -4,6 +4,15 @@ const readLead = async () => {
     const query = "SELECT * FROM leads";
     const [rows] = await db.promise().query(query);
     return rows;
-}
+};
 
-module.exports = { readLead };
+const createNewLead = async ({ name, email, mobile, company, source, status }) => {
+    const query = "INSERT INTO leads (name, email, mobile, company, source, status) VALUES (?, ?, ?, ?, ?, ?)";
+    const [rows] = await db.promise().query(query, [name, email, mobile, company, source, status]);
+    return rows;
+};
+
+module.exports = {
+    readLead,
+    createNewLead,
+};
