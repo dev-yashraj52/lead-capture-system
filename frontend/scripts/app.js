7217818786ccf7011d2500c7cac226b812d760a3
Yash Raj <dev.yashraj52@gmail.com>
2026-06-06 23:20:56 +0530

    add modal hide/unhide using button

---
+++ frontend/scripts/app.js
@@ -0,0 +1,17 @@
+const leadModal = document.getElementById('leadModal');
+const leadForm = document.getElementById('leadForm')
+const closeModalButton = document.getElementById('closeModalBtn');
+const addLeadBtn = document.getElementById('addLeadBtn');
+const cancelBtn = document.getElementById('cancelBtn');
+
+addLeadBtn.addEventListener('click', () => openModal());
+closeModalButton.addEventListener('click', () => closeModal());
+
+function openModal() {
+    leadForm.reset();
+    leadModal.style.display = "flex";
+}
+
+function closeModal() {
+    leadModal.style.display = "none";
+}
